e6fa3e792db600c33820fc8e78dcb554405906cf
Your New GitHub Username <farrukh_3@outlook.com>
2025-06-06 00:32:20 +0500

    doubleClickHandlingPractice

--- justPractice/doubleClickPractice.py
+++ justPractice/doubleClickPractice.py
@@ -1,5 +1,5 @@
 from selenium import webdriver
-from selenium.webdriver import ActionChains
+from selenium.webdriver import ActionChains, Keys
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 import time
@@ -10,6 +10,9 @@ driver.maximize_window()
 wait=WebDriverWait(driver,10)
 
 driver.get("https://testautomationpractice.blogspot.com/")
+field1=driver.find_element(By.XPATH, "(//input[@id='field1'])[1]")
+field1.clear()
+field1.send_keys("newData")
 copyButton=driver.find_element(By.XPATH, "//button[@ondblclick='myFunction1()']")
 
 act=ActionChains(driver)
